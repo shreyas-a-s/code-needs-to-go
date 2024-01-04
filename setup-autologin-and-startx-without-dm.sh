eb983e7a49774f45ba8e8709876f4f1510b21219
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-04 21:23:37 +0530

    Separate 'Enable automatic startx' from ip statement

--- setup-autologin-and-startx-without-dm.sh
+++ setup-autologin-and-startx-without-dm.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 
+# Enable autologin to tty
 if [ -f /etc/inittab ]; then # For non-systemd distros
-  # Enable autologin
   sudo cp /etc/inittab /etc/inittab.bak
   sed -i '/1:2345/s/^/#/' /etc/inittab
   sed -i "/1:2345/a\1:2345:respawn:/bin/login -f $USER tty1 </dev/tty1 >/dev/tty1 2>&1" /etc/inittab
-
-  # Enable automatic startx
-  printf "\
-  \nif [ -z "\$DISPLAY" ] && [ \$(tty) == /dev/tty1 ]; then\
-  \n  startx\
-  \nfi" >> ~/.bash_profile
 else # For systemd distros
   [ -d /etc/systemd/system/getty@tty1.service.d ] || sudo mkdir /etc/systemd/system/getty@tty1.service.d
   printf "[Service]\
@@ -18,4 +12,10 @@ else # For systemd distros
   \nExecStart=-/sbin/agetty -o '-p -f -- \\\\\\\\u' --noclear --autologin $USER %%I \$TERM\
   \n" | sudo tee /etc/systemd/system/getty@tty1.service.d/autologin.conf > /dev/null
 fi
+
+# Enable automatic startx
+printf "\
+\nif [ -z "\$DISPLAY" ] && [ \$(tty) == /dev/tty1 ]; then\
+\n  startx\
+\nfi" >> ~/.bash_profile
 
