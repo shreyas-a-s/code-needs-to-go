5e1ff5caa86a267194ec743730907dffb360e5f2
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-19 00:47:34 +0530

    Add install-jb2.sh & split-fasta-and-jb2-prepare.sh

---
+++ install-jb2.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Install dependencies
+sudo apt install -y samtools tabix wget curl apache2
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+nvm install 16
+
+# Installation
+npm install -g @jbrowse/cli
+jbrowse create jbrowse2
+sudo chown -R $USER:$USER /var/www/html/
+cp -r jbrowse /var/www/html/
+
+# Setup sample dataset
+mkdir data
+cd data/
+
---
+++ split-fasta-and-jb2-prepare.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+### Source ###
+# https://www.biostars.org/p/13270/#13281
+
+file="$1"
+filename_without_ext=$(basename "$file" | sed 's/\.[^.]*$//')
+
+awk 'BEGIN {n_seq=0; num=0; max_size=2*1024*1024*1024;} /^>/ {if(n_seq%4000000000000==0 || size > max_size){file=sprintf("%s-%d.fa",substr(FILENAME,1,length(FILENAME)-4),num++); size=0;} print >> file; n_seq++; next;} { size+=length($0) + 1; print >> file; }' "$file"
+
+find . -iname '*.fa' -exec samtools faidx \{\} \;
+
+find . -iname '*.fa' -print0 | sort -z | xargs -0 -I {} sh -c "jbrowse add-assembly '{}' --load copy --out $filename_without_ext"
+
+find . -iname '*.fa' -exec rm \{\} \;
+find . -iname '*.fai' -exec rm \{\} \;
+
