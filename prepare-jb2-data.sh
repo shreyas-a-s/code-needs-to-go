c693d817f008941608ba1e812be5f78f669e3280
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-06-07 17:27:09 +0530

    fix(prepare-jb2-data.sh): add missing indexing step of gff

--- prepare-jb2-data.sh
+++ prepare-jb2-data.sh
@@ -3,6 +3,7 @@
 samtools faidx nelumbo-nucifera.fna
 jbrowse add-assembly nelumbo-nucifera.fna --load copy --out nelumbo-nucifera
 jbrowse sort-gff nelumbo-nucifera.gff | bgzip > nelumbo-nucifera.sorted.gff.gz
+tabix nelumbo-nucifera.sorted.gff.gz
 jbrowse add-track nelumbo-nucifera.sorted.gff.gz --load copy --out nelumbo-nucifera
 cd nelumbo-nucifera
 jbrowse text-index
